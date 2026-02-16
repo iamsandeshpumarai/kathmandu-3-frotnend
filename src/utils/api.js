fb9835051b83d333956ca19ef072156a00865da4
Sandesh RaI <pumasandeshrai@gmail.com>
2026-02-16 14:59:59 +0545

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   // You must specify 'baseURL' as the key
   // baseURL: "https://survey2-1-jjdd.onrender.com",
-  baseURL:'http://localhost:5000',
+  baseURL:'https://chitwanbackend1.onrender.com/',
   // baseURL: "https://survey-api.kantipurride.com/",
   
   withCredentials: true
