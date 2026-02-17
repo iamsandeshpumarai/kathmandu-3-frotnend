98ae6dc3b382734a7f798d9ca9b5b06fa93c2f32
Sandesh Rai <pumasandeshrai@gmail.com>
2026-02-17 12:16:13 +0545

    updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,9 +4,9 @@ const api = axios.create({
   // You must specify 'baseURL' as the key
   // baseURL: "https://survey2-1-jjdd.onrender.com",
 
-  baseURL:'https://chitwanbackend1.onrender.com',
+  // baseURL:'https://chitwanbackend1.onrender.com',
   // baseURL: "https://survey-api.kantipurride.com/",
-  
+  baseURL:"http://localhost:5000/",
   withCredentials: true
 });
 
